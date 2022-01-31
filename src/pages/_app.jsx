a2374a8f4cd8a05210b13980521102fb96cd79a6
Gregor Adams <greg@pixelass.com>
2022-01-31 10:26:52 +0100

    chore: setup global styles

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,23 @@
+import { Global } from "@emotion/react";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./";
 import About from "./about";
+import { globalStyle } from "../ions/styles";
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/">
-					<Route index element={<Home />} />
-					<Route path="about/" element={<About />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<>
+			<Global styles={globalStyle} />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+						<Route path="about/" element={<About />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 };
 
